165f7f40c9d4e72f9fc8d9bd70e1402f4254a13b
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-10-25 20:16:19 +0900

    feat : MusicKnob Practice
    - MotionEvent, Canvas 에 대한 이해
    - modifier

--- app/src/main/java/com/bendeng/composepractice/MainActivity.kt
+++ app/src/main/java/com/bendeng/composepractice/MainActivity.kt
@@ -3,13 +3,9 @@ package com.bendeng.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.bendeng.composepractice.component.CircularProgressBarPractice
+import com.bendeng.composepractice.practice.MusicKnobScreen
 import com.bendeng.composepractice.ui.theme.ComposePracticeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -20,12 +16,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposePracticeTheme {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    CircularProgressBarPractice(percentage = 0.8f, number = 100)
-                }
+                MusicKnobScreen()
             }
         }
     }
