1542a1420dca9b50f852d638cc94c16a7a1bf8be
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-11-07 18:43:35 +0900

    feat : 3d Dropdown 구현

--- app/src/main/java/com/bendeng/composepractice/MainActivity.kt
+++ app/src/main/java/com/bendeng/composepractice/MainActivity.kt
@@ -3,9 +3,20 @@ package com.bendeng.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.*
-import com.bendeng.composepractice.practice.MeditationScreen.MeditationScreen
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bendeng.composepractice.component.DropDown3D
 import com.bendeng.composepractice.ui.theme.ComposePracticeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,11 +27,24 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposePracticeTheme {
-                MeditationScreen()
+                Surface(
+                    color = Color(0xFF101010),
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    DropDown3D(
+                        text = "hello",
+                        modifier = Modifier.padding(16.dp),
+                        initiallyOpened = true,
+                    ) {
+                        Text(text = "this is now revealed!",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .heightIn(100.dp)
+                                .background(Color.Green))
+                    }
+
+                }
             }
         }
     }
 }
-
-
-
